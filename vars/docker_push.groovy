795fe0deb602fa68f078eaeb9e738c77406deca3
Hritik Ranjan <141608197+hritikranjan1@users.noreply.github.com>
2026-09-06 13:52:57 +0530

    Enhance docker_push function for secure login
    Refactor docker push function to use password-stdin for login and include logout.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,17 @@
-def call (String Project , String ImageTag , String DockerHubUser){
-  withCredentials([usernamePassword(credentialsId: 'dockerhubcred' , passwordVariable: 'DOCKERHUB_USER' , usernameVariable: 'DOCKERHUB_PASS')]) {
-    sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+def call(String Project, String ImageTag, String DockerHubUser) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerhubcred',
+            usernameVariable: 'DOCKERHUB_USER',
+            passwordVariable: 'DOCKERHUB_PASS'
+        )
+    ]) {
+
+        sh """
+            echo "\$DOCKERHUB_PASS" | docker login -u "\$DOCKERHUB_USER" --password-stdin
+            docker push ${DockerHubUser}/${Project}:${ImageTag}
+            docker logout
+        """
+    }
 }
